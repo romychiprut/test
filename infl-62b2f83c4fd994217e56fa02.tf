d0b9b38ca4e4a51f24ae02f69d562c1711e148b3
romychiprut <92666512+romychiprut@users.noreply.github.com>
2022-06-22 14:08:46 +0300

    Codification: aws_ssm_parameter

---
+++ infl-62b2f83c4fd994217e56fa02.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "aws_ssm_parameter_scxdnh" {
+  arn   = "arn:aws:ssm:us-west-2:094724549126:parameter/stag-flywheel-fetcher-producer-chunks"
+  name  = "stag-flywheel-fetcher-producer-chunks"
+  type  = "String"
+  value = "REDACTED-BY-FIREFLY:caddb07b916ca7a4d8339a2d4ee7c8c1ca6a5c7d97522aa02ba0bee168c5817f:sha256"
+}
+
